f8004fea80b50d0f25273b2df8b28c6c03afad37
rupom <furqanrupom978@gmail.com>
2023-08-05 01:16:02 +0600

    rebuild our hero section and make it responsive as well

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,42 +1,37 @@
+import React from "react";
+import { FaDumbbell } from 'react-icons/fa';
+import { IoIosArrowRoundForward } from 'react-icons/io';
 import Image from "next/image";
-import bg from "../assets/gym.png";
-import logo from '../assets/body-building.png'
+import bg from "../../public/bodybuilder.jpg";
+import logo from '../assets/body-building.png';
 
 const Hero = () => {
   return (
-    <section className="h-screen relative pt-14" style={{
-      backgroundImage: `url(${bg.src})`,
-      width: '100%',
-      height: '100vh',
-      backgroundSize:'cover'
-    }}>
+    <section className="h-screen relative flex items-center overflow-hidden">
+      <div className="absolute inset-0 z-0">
+        <Image className="pt-14" src={bg} alt="background" fill objectFit="cover" quality={100} />
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      </div>
 
-      {/* add image and other things for banner section  */}
-
-
-      <div className="pt-12 ml-12 ">
-        <div>
-      <Image className="pl-5" ref={null} src={logo} alt="body-builder" />
-      <h3 className=" pl-5 text-md font-semibold text-white ">Fitness <br />revolution</h3>
-
-      <h1 className="text-5xl lg:text-7xl pt-8  md:pt-40 pb-8  font-bold text-white">
-        Start your
-      </h1>
-      <h1 className="text-5xl lg:text-7xl -mt-4 text-rose-900">
-        training today
-      </h1>
+      <div className="z-10 mx-auto text-center text-white">
+        <div className="flex items-center flex-col mb-6">
+          <Image className="pl-5" src={logo} alt="body-builder" width={100} height={100} />
+          <h3 className="pl-5 text-lg font-semibold">Fitness Revolution</h3>
         </div>
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-6">
+          Start your  training <span className="text-rose-500"> today</span>
+        </h1>
       </div>
 
-      <div className="absolute bottom-0 right-0">
-        <button className="px-12 pr-32 text-xl  py-3 bg-rose-500 text-white rounded-tl-full rounded-bl-full">Join Now</button>
+      <div className="absolute bottom-[20%] left-[50%] transform -translate-x-1/2 m-8">
+        <button className="flex items-center justify-center  px-4 sm:px-8 md:px-12 py-3 md:py-4 text-lg md:text-xl bg-gradient-to-r from-rose-500 to-rose-600 text-white rounded-full shadow-lg hover:shadow-xl transition duration-300 transform hover:scale-105">
+          <FaDumbbell className="mr-2 text-xl" />
+          Join Now
+          <IoIosArrowRoundForward className="ml-2 text-xl" />
+        </button>
       </div>
-
     </section>
   );
 };
 
 export default Hero;
-
-
-
