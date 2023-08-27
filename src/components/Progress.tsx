c90e6d5b2357f34c4ff5d42fd04b3520bf162882
rupom <furqanrupom978@gmail.com>
2023-08-28 02:18:05 +0600

    changes again

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,81 +1,82 @@
 import Image from "next/image";
 
 const Progress = () => {
-    return (
-        <section className="mt-32 xl:mt-48 mb-16 px-12 lg:px-0">
-            <h1 className=" text-center text-3xl xl:text-5xl font-bold ">
-                Our Fitness <span className="text-rose-500">Plans</span>
-            </h1>
-            <p className="max-w-2xl text-sm text-gray-600 mx-auto leading-relaxed text-center pt-3">Tailored strategies that guide individuals through exercises, nutrition, and lifestyle adjustments, all aimed at achieving optimal health and wellness</p>
+  return (
+    <section className="mt-32 xl:mt-48 mb-16 px-12 lg:px-0">
+      <h1 className=" text-center text-3xl xl:text-5xl font-bold ">
+        Our Fitness <span className="text-rose-500">Plans</span>
+      </h1>
+      <p className="max-w-2xl text-sm text-gray-600 mx-auto leading-relaxed text-center pt-3">
+        Tailored strategies that guide individuals through exercises, nutrition,
+        and lifestyle adjustments, all aimed at achieving optimal health and
+        wellness
+      </p>
 
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16">
+        <div className="hover:scale-105 duration-500 group">
+          <Image
+            width={500}
+            height={500}
+            src="https://images.pexels.com/photos/17542096/pexels-photo-17542096/free-photo-of-bearded-man-in-white-t-shirt-lifting-weight-at-gym.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="gym day"
+          />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16">
-                <div className="hover:scale-105 duration-500 group">
-                    <Image  width={500} height={500} src="https://images.pexels.com/photos/17542096/pexels-photo-17542096/free-photo-of-bearded-man-in-white-t-shirt-lifting-weight-at-gym.jpeg?auto=compress&cs=tinysrgb&w=600" alt="gym day" />
+          <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
+            <p>Coaching</p>
+            <div className="w-2 h-2  rounded-full bg-rose-500"></div>
+            <p>Trancing</p>
+          </div>
 
-                    <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
-                        <p>Coaching</p>
-                        <div className="w-2 h-2  rounded-full bg-rose-500"></div>
-                        <p>Trancing</p>
-                    </div>
+          <h2 className="text-2xl font-bold py-3">Cardio Fitness Session</h2>
 
-                    <h2 className="text-2xl font-bold py-3">
-                    Cardio Fitness Session
-                    </h2>
+          <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
+            <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500"></div>
+          </div>
+        </div>
 
-                    <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
-                        <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500">
+        <div className="hover:scale-105 duration-500 group md:order-3 lg:order-none">
+          <Image
+            width={500}
+            height={500}
+            src="https://images.pexels.com/photos/9283300/pexels-photo-9283300.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="gym day"
+          />
 
-                        </div>
-                    </div>
-                </div>
+          <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
+            <p>Coaching</p>
+            <div className="w-2 h-2  rounded-full bg-rose-500"></div>
+            <p>Trancing</p>
+          </div>
+          <h2 className="text-2xl font-bold py-3">Movement and Fitness Day</h2>
 
-                <div className="hover:scale-105 duration-500 group md:order-3 lg:order-none">
-                <Image width={500} height={500} src="https://images.pexels.com/photos/9283300/pexels-photo-9283300.jpeg?auto=compress&cs=tinysrgb&w=600" alt="gym day" />
+          <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
+            <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500"></div>
+          </div>
+        </div>
 
-                <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
-                        <p>Coaching</p>
-                        <div className="w-2 h-2  rounded-full bg-rose-500"></div>
-                        <p>Trancing</p>
+        <div className="hover:scale-105 duration-500 group">
+          <Image
+            width={500}
+            height={500}
+            src="https://images.pexels.com/photos/6456159/pexels-photo-6456159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="gym day"
+          />
 
-                    </div>
-                    <h2 className="text-2xl font-bold py-3">
-                    Movement and Fitness Day
-                    </h2>
+          <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
+            <p>Coaching</p>
+            <div className="w-2 h-2  rounded-full bg-rose-500"></div>
+            <p>Challenge</p>
+          </div>
 
-                    <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
-                        <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500">
+          <h2 className="text-2xl font-bold py-3">Structural Mastery</h2>
 
-                        </div>
-                    </div>
-
-
-                </div>
-
-
-                <div className="hover:scale-105 duration-500 group">
-                <Image width={500} height={500} src="https://images.pexels.com/photos/6456159/pexels-photo-6456159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="gym day" />
-
-
-                <div className="uppercase  text-sm flex items-center space-x-2 font-bold pt-3">
-                        <p>Coaching</p>
-                        <div className="w-2 h-2  rounded-full bg-rose-500"></div>
-                        <p>Challenge</p>
-                    </div>
-
-                    <h2 className="text-2xl font-bold py-3">
-                    Structural Mastery
-                    </h2>
-
-                    <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
-                        <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="w-full  h-2 bg-gray-200  mt-4 rounded-full">
+            <div className="w-0 bg-rose-500 h-full group-hover:w-full rounded-full duration-500"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Progress;
--- src/components/Raise.tsx
+++ src/components/Raise.tsx
@@ -1,48 +1,93 @@
 import Image from "next/image";
-import {MdVerifiedUser} from 'react-icons/md'
+import { MdVerifiedUser } from "react-icons/md";
 
 const Raise = () => {
-    return (
-        <section className="px-8 lg:px-0 mt-32 mb-16 grid grid-cols-1 justify-items-center lg:justify-items-start lg:grid-cols-2 ">
-            <div className="relative">
-                <Image className="z-20" src="https://images.pexels.com/photos/1756959/pexels-photo-1756959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={500} height={500}  alt="raise picture"/>
+  return (
+    <section className="px-8 lg:px-0 mt-32 mb-16 grid grid-cols-1 justify-items-center lg:justify-items-start lg:grid-cols-2 ">
+      <div className="relative">
+        <Image
+          className="z-20"
+          src="https://images.pexels.com/photos/1756959/pexels-photo-1756959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          width={500}
+          height={500}
+          alt="raise picture"
+        />
 
-                <div className="absolute hidden lg:block  -z-10 -right-[40%] -bottom-6">
-                <Image src="https://images.pexels.com/photos/1693968/pexels-photo-1693968.jpeg?auto=compress&cs=tinysrgb&w=400&h=300&dpr=1" width={500} height={500}  alt="raise picture"/>
+        <div className="absolute hidden lg:block  -z-10 -right-[40%] -bottom-6">
+          <Image
+            src="https://images.pexels.com/photos/1693968/pexels-photo-1693968.jpeg?auto=compress&cs=tinysrgb&w=400&h=300&dpr=1"
+            width={500}
+            height={500}
+            alt="raise picture"
+          />
+        </div>
+      </div>
 
-                </div>
-            </div>
+      <div className="md:ml-28">
+        <h1 className="text-2xl md:text-3xl pt-12 lg:pt-4 uppercase font-bold">
+          We boost your self-assurance
+        </h1>
 
-
-            <div className="md:ml-28">
-                <h1 className="text-2xl md:text-3xl pt-12 lg:pt-4 uppercase font-bold">We boost your self-assurance</h1>
-
-
-                <ul className="lg:pl-12 pt-12 flex flex-col space-y-6  text-sm">
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p>Regularly provide sincere and constructive feedback on their strengths and accomplishments.</p>
-                    </li>
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p>Help them set realistic goals that are challenging yet attainable. The process of achieving.</p>
-                    </li>
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p> Taking care of their mental and physical well-being can improve their overall confidence.</p>
-                    </li>
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p>Guide them through visualization exercises where they imagine themselves succeeding in various situations.</p>
-                    </li>
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p>Cultivate gratitude by focusing on the positive aspects of their life.</p>
-                    </li>
-                    <li className="flex items-center space-x-2">
-                        <span className="text-2xl text-rose-500"><MdVerifiedUser /></span> <p>Encourage them to step out of their comfort zones gradually. Each new experience can contribute to their sense of capability.</p>
-                    </li>
-                </ul>
-            </div>
-
-
-        </section>
-    );
+        <ul className="lg:pl-12 pt-12 flex flex-col space-y-6  text-sm">
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              Regularly provide sincere and constructive feedback on their
+              strengths and accomplishments.
+            </p>
+          </li>
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              Help them set realistic goals that are challenging yet attainable.
+              The process of achieving.
+            </p>
+          </li>
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              {" "}
+              Taking care of their mental and physical well-being can improve
+              their overall confidence.
+            </p>
+          </li>
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              Guide them through visualization exercises where they imagine
+              themselves succeeding in various situations.
+            </p>
+          </li>
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              Cultivate gratitude by focusing on the positive aspects of their
+              life.
+            </p>
+          </li>
+          <li className="flex items-center space-x-2">
+            <span className="text-2xl text-rose-500">
+              <MdVerifiedUser />
+            </span>{" "}
+            <p>
+              Encourage them to step out of their comfort zones gradually. Each
+              new experience can contribute to their sense of capability.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Raise;
